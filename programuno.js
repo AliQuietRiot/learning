b5ed8e97f0593785059cf0850bc0e1e580083884
fox <fox@asus.Home>
2018-01-26 19:36:42 +0000

    adding first lesson

---
+++ programuno.js
@@ -0,0 +1,23 @@
+// This is my first program 
+console.log('Oh hi') // strings are enclosed in quotes
+
+var str = 'This is a string, a string is a thing' //here it is
+
+var num = 3 //dis a numba
+
+while (num > 0) { // while num is greater than 0
+  console.log(str)
+  console.log(num)
+  num-- // -- is a shorthand for var = var-1
+}
+
+
+var thing = 34
+
+if (thing === 34) {
+  console.log('Thing is 34!') //this is a positive outcome, (thing === 34) evaluated true 
+}  else {
+  Console.log('Aw noo!') //this is negative outcome, (thing === 34) evaluated false
+}
+
+
